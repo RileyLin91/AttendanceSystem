42acc7d0ae13522ebd340b13449269c81b2a3148
RileyLin91 <41749800+RileyLin91@users.noreply.github.com>
2020-11-12 11:18:29 +0800

    Design title user interface

--- AttendanceSysForCSharp/AttendanceSysForCSharp/fm_Title.Designer.cs
+++ AttendanceSysForCSharp/AttendanceSysForCSharp/fm_Title.Designer.cs
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtbx_Description = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.btn_Insert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.rtbx_Description);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(13, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 201);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "職位管理";
+            // 
+            // rtbx_Description
+            // 
+            this.rtbx_Description.Location = new System.Drawing.Point(133, 120);
+            this.rtbx_Description.Name = "rtbx_Description";
+            this.rtbx_Description.Size = new System.Drawing.Size(624, 65);
+            this.rtbx_Description.TabIndex = 42;
+            this.rtbx_Description.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(26, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "描述";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(26, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "職位名稱";
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.btn_Cancel.Location = new System.Drawing.Point(722, 21);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(67, 39);
+            this.btn_Cancel.TabIndex = 6;
+            this.btn_Cancel.Text = "取消";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.btn_Edit.Location = new System.Drawing.Point(649, 21);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(67, 39);
+            this.btn_Edit.TabIndex = 5;
+            this.btn_Edit.Text = "修改";
+            this.btn_Edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_Insert
+            // 
+            this.btn_Insert.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.btn_Insert.Location = new System.Drawing.Point(574, 21);
+            this.btn_Insert.Name = "btn_Insert";
+            this.btn_Insert.Size = new System.Drawing.Size(67, 39);
+            this.btn_Insert.TabIndex = 4;
+            this.btn_Insert.Text = "新增";
+            this.btn_Insert.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(26, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "部門";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(133, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(271, 27);
+            this.textBox1.TabIndex = 44;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(133, 78);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(271, 24);
+            this.comboBox1.TabIndex = 45;
+            // 
+            // fm_Title
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "fm_Title";
+            this.ClientSize = new System.Drawing.Size(810, 585);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.btn_Insert);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "fm_Title";
+            this.Text = "職位管理";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtbx_Description;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.Button btn_Edit;
+        private System.Windows.Forms.Button btn_Insert;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
