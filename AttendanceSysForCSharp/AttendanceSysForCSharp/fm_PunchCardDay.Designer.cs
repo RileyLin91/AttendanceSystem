f634c3edd95d9b6565dbcea50048810425c80aab
RileyLin91 <41749800+RileyLin91@users.noreply.github.com>
2020-11-12 11:31:54 +0800

    Design PunchCardDay form and PunchCardMonth form user interface

--- AttendanceSysForCSharp/AttendanceSysForCSharp/fm_PunchCardDay.Designer.cs
+++ AttendanceSysForCSharp/AttendanceSysForCSharp/fm_PunchCardDay.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_OffWork = new System.Windows.Forms.Button();
+            this.btn_Work = new System.Windows.Forms.Button();
+            this.dt_Date = new System.Windows.Forms.DateTimePicker();
+            this.nup_Hour = new System.Windows.Forms.NumericUpDown();
+            this.nup_Minute = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nup_Hour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nup_Minute)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(28, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "打卡日期";
+            // 
+            // btn_OffWork
+            // 
+            this.btn_OffWork.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.btn_OffWork.Location = new System.Drawing.Point(572, 23);
+            this.btn_OffWork.Name = "btn_OffWork";
+            this.btn_OffWork.Size = new System.Drawing.Size(67, 39);
+            this.btn_OffWork.TabIndex = 7;
+            this.btn_OffWork.Text = "下班";
+            this.btn_OffWork.UseVisualStyleBackColor = true;
+            // 
+            // btn_Work
+            // 
+            this.btn_Work.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.btn_Work.Location = new System.Drawing.Point(497, 23);
+            this.btn_Work.Name = "btn_Work";
+            this.btn_Work.Size = new System.Drawing.Size(67, 39);
+            this.btn_Work.TabIndex = 6;
+            this.btn_Work.Text = "上班";
+            this.btn_Work.UseVisualStyleBackColor = true;
+            // 
+            // dt_Date
+            // 
+            this.dt_Date.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dt_Date.Location = new System.Drawing.Point(122, 27);
+            this.dt_Date.Name = "dt_Date";
+            this.dt_Date.Size = new System.Drawing.Size(178, 27);
+            this.dt_Date.TabIndex = 39;
+            // 
+            // nup_Hour
+            // 
+            this.nup_Hour.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.nup_Hour.Location = new System.Drawing.Point(306, 28);
+            this.nup_Hour.Name = "nup_Hour";
+            this.nup_Hour.Size = new System.Drawing.Size(46, 27);
+            this.nup_Hour.TabIndex = 40;
+            // 
+            // nup_Minute
+            // 
+            this.nup_Minute.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.nup_Minute.Location = new System.Drawing.Point(358, 28);
+            this.nup_Minute.Name = "nup_Minute";
+            this.nup_Minute.Size = new System.Drawing.Size(46, 27);
+            this.nup_Minute.TabIndex = 41;
+            // 
+            // fm_PunchCardDay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "fm_PunchCardDay";
+            this.ClientSize = new System.Drawing.Size(688, 450);
+            this.Controls.Add(this.nup_Minute);
+            this.Controls.Add(this.nup_Hour);
+            this.Controls.Add(this.dt_Date);
+            this.Controls.Add(this.btn_OffWork);
+            this.Controls.Add(this.btn_Work);
+            this.Controls.Add(this.label3);
+            this.Name = "fm_PunchCardDay";
+            this.Text = "打卡(日)";
+            ((System.ComponentModel.ISupportInitialize)(this.nup_Hour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nup_Minute)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_OffWork;
+        private System.Windows.Forms.Button btn_Work;
+        private System.Windows.Forms.DateTimePicker dt_Date;
+        private System.Windows.Forms.NumericUpDown nup_Hour;
+        private System.Windows.Forms.NumericUpDown nup_Minute;
     }
 }
--- AttendanceSysForCSharp/AttendanceSysForCSharp/fm_PunchCardMonth.Designer.cs
+++ AttendanceSysForCSharp/AttendanceSysForCSharp/fm_PunchCardMonth.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dt_StartDate = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Export = new System.Windows.Forms.Button();
+            this.btn_Store = new System.Windows.Forms.Button();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dt_EndDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // dt_StartDate
+            // 
+            this.dt_StartDate.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dt_StartDate.Location = new System.Drawing.Point(108, 19);
+            this.dt_StartDate.Name = "dt_StartDate";
+            this.dt_StartDate.Size = new System.Drawing.Size(143, 27);
+            this.dt_StartDate.TabIndex = 41;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(21, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "開始日期";
+            // 
+            // btn_Export
+            // 
+            this.btn_Export.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.btn_Export.Location = new System.Drawing.Point(677, 15);
+            this.btn_Export.Name = "btn_Export";
+            this.btn_Export.Size = new System.Drawing.Size(101, 39);
+            this.btn_Export.TabIndex = 44;
+            this.btn_Export.Text = "匯出Excel";
+            this.btn_Export.UseVisualStyleBackColor = true;
+            // 
+            // btn_Store
+            // 
+            this.btn_Store.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.btn_Store.Location = new System.Drawing.Point(604, 15);
+            this.btn_Store.Name = "btn_Store";
+            this.btn_Store.Size = new System.Drawing.Size(67, 39);
+            this.btn_Store.TabIndex = 43;
+            this.btn_Store.Text = "儲存";
+            this.btn_Store.UseVisualStyleBackColor = true;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Font = new System.Drawing.Font("PMingLiU", 12F);
+            this.btn_Search.Location = new System.Drawing.Point(529, 15);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(67, 39);
+            this.btn_Search.TabIndex = 42;
+            this.btn_Search.Text = "查詢";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(257, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "至";
+            // 
+            // dt_EndDate
+            // 
+            this.dt_EndDate.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.dt_EndDate.Location = new System.Drawing.Point(287, 19);
+            this.dt_EndDate.Name = "dt_EndDate";
+            this.dt_EndDate.Size = new System.Drawing.Size(143, 27);
+            this.dt_EndDate.TabIndex = 46;
+            // 
+            // fm_PunchCardMonth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "fm_PunchCardMonth";
+            this.Controls.Add(this.dt_EndDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Export);
+            this.Controls.Add(this.btn_Store);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.dt_StartDate);
+            this.Controls.Add(this.label3);
+            this.Name = "fm_PunchCardMonth";
+            this.Text = "打卡(月)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker dt_StartDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Export;
+        private System.Windows.Forms.Button btn_Store;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dt_EndDate;
     }
 }
